97b35d0261bc56c8fe7c0f666d80e11ae33c64e9
Filip-Mostecky <piuat.fik@seznam.cz>
2025-05-07 11:46:49 +0200

    Update alarm_buzzer.vhd

--- Digital_clock/hodiny_v7.srcs/sources_1/new/alarm_buzzer.vhd
+++ Digital_clock/hodiny_v7.srcs/sources_1/new/alarm_buzzer.vhd
@@ -4,13 +4,13 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity alarm_buzzer is
     Port (
         alarm    : in  STD_LOGIC;   -- Vstupní signál alarmu (1 = aktivní)
-        clk      : in  STD_LOGIC;   -- Hodinový signál 20 Hz
+        clk      : in  STD_LOGIC;   -- Hodinový signál
         buzzer   : out STD_LOGIC    -- Výstup pro bzučák
     );
 end alarm_buzzer;
 
 architecture Behavioral of alarm_buzzer is
 begin
-    -- Bzučák je aktivován pouze při alarmu a kmitá na 20 Hz
+    -- Bzučák je aktivován pouze při alarmu a kmitá na frekvenci clk
     buzzer <= alarm and clk;
 end Behavioral;
